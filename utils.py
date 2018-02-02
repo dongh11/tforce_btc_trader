2766da0ea933397f83df620db2fec41d7707b9f2
Tyler Renelle <tylerrenelle@gmail.com>
2018-02-02 10:09:20 -0800

    Misc updates

--- utils.py
+++ utils.py
@@ -38,8 +38,8 @@ def calculate_score(advantages):
 def add_common_args(parser):
     parser.add_argument('-g', '--gpu-split', type=float, default=1, help="Num ways we'll split the GPU (how many tabs you running?)")
     parser.add_argument('-n', '--net-type', type=str, default='conv2d')
-    parser.add_argument('-t', '--n-tests', type=int, default=30, help="Number of times to split to training and run a test. This slows things down, so balance graph resolution w/ performance.")
-    parser.add_argument('-s', '--n-steps', type=int, default=300, help="Number of thousands of timesteps total to train.")
+    parser.add_argument('-t', '--n-tests', type=int, default=100, help="Number of times to split to training and run a test. This slows things down, so balance graph resolution w/ performance.")
+    parser.add_argument('-s', '--n-steps', type=int, default=200, help="Number of 1k timesteps total to train. (using 50 means 500,000)")
 
 
 # One array per running instance (ie, if you have 2 separate tabs running hypersearch.py, then you'll want an array of
@@ -47,20 +47,22 @@ def add_common_args(parser):
 # second array
 guess_overrides = [
     [
-        {},  # usually want 1 empty dict, which means "try the hard-coded defaults"
-        {'net.depth_post': 2},
+        {'discount': .95},
+        {'update_mode.batch_size': 10},
+        {'net.l2': 7., 'net.l1': 3.},  # only l1
+        {'net.l1': 7., 'net.l2': 7.},  # off
         {'pct_change': False},
-        {'net.l1': 3},
-        {'net.l2': 3},
-        {'net.dropout': .5},
+        {'net.depth_post': 2},
+        {'step_optimizer.learning_rate': 5.5},
     ],
     [
         {'single_action': False},
-        {'repeat_last_state': True},
         {'punish_repeats': 5000},
         {'net.width': 4},
         {'net.width': 8},
-        {'step_optimizer.learning_rate': 7},
+        {'net.l1': 4., 'net.l2': 4.},  # both
+        {'repeat_last_state': True},
+        {},  # usually want 1 empty dict, which means "try the hard-coded defaults"
     ]
 ]
 
